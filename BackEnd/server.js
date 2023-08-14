594f008a6b471532aa35b16f3c33b04210c49971
Jakob Beber <89211306@student.upr.si>
2023-08-14 21:49:53 +0200

    feat: get/post functions for database and frontend communication

---
+++ BackEnd/server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+//import db from './database.js';
+
+const app = express();
+
+app.use(express.json());
+
+//routes
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+
+
+app.listen(3000, () => {
+  console.log('server is running on port 3000');
+});
